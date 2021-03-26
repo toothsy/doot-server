882907e8d35fa57a84507b5f6a1b612fe6aee19b
toothsy <c.j.atharva@gmail.com>
2021-03-26 17:52:27 +0530

    heroku deply 4

--- server.js
+++ server.js
@@ -11,21 +11,18 @@ let userData = [
     }
 ]
 const verify = (req, res) =>{
-    console.log(req.body);
     let username = req.body.username
     let password = req.body.password
     let flag =  userData.some(user=>user.username === username&&user.password === password )
-    console.log(flag)
     flag? res.status(200).send(true): res.send(false).status(200)     
                 }
 const register = (req, res) =>{
     userData.push(req.body)
-    console.log(userData)
     res.status(200).send(true)
 }        
 const port = process.env.PORT||5000;
 app.use(express.json({ extended: false }) );
 app.post('/login',verify)
 app.post('/register', register)
-app.get('/home', (req, res) =>console.log("this is home"))
+app.get('/', (req, res) =>res.send("nooo dont come here"))
 app.listen(port,()=>{console.log("server is running on port " + port)})
